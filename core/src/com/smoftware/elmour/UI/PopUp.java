c724fde0acfd637ed3ce2b6ac91c9a61f0a87749
smoenssen <steve.moenssen@gmail.com>
2017-09-17 21:49:44 -0400

    got popup text displaying char by char with temp delay

--- core/src/com/smoftware/elmour/UI/PopUp.java
+++ core/src/com/smoftware/elmour/UI/PopUp.java
@@ -12,6 +12,7 @@ import com.smoftware.elmour.Utility;
  */
 
 public class PopUp extends Window {
+    private static final String TAG = PopUp.class.getSimpleName();
 
     String text;
     TextArea textArea;
@@ -65,9 +66,117 @@ public class PopUp extends Window {
         else {
             FileHandle file = Gdx.files.internal(filePath);
             text = file.readString();
-            Gdx.app.debug("tag", "file text = " + text);
+            Gdx.app.debug(TAG, "file text = " + text);
         }
 
-        textArea.setText(text);
+        Runnable r = new Runnable() {
+            public void run() {
+                String pendingText = "";
+                String currentVisibleText = "";
+                char currentChar = ' ';
+                boolean isEndOfLine = false;
+                boolean addSpaceToPendingText = false;
+                int numLines = 0;
+
+                for (int i = 0; i < text.length(); i++) {
+
+                    if (addSpaceToPendingText) {
+                        pendingText += ' ';
+                        addSpaceToPendingText = false;
+                    }
+
+                    if (!isEndOfLine) {
+                        currentChar = text.charAt(i);
+                        pendingText += currentChar;
+                        textArea.setText(pendingText);
+                        numLines = textArea.getLines();
+                    }
+                    else {
+                        // back up to previous character
+                        isEndOfLine = false;
+                        i--;
+                    }
+
+                    if (numLines > 2 || currentChar == '\n') {
+                        // done populating current box
+                        // wrapped to third line so need to pause for next request
+                        textArea.setText(currentVisibleText);
+
+                        //Gdx.app.log(TAG, "pending text = " + pendingText);
+                        //Gdx.app.log(TAG, String.format("numLines = %d, currentaChar = %c ", numLines, currentChar));
+                        try {
+                            Thread.sleep(1000);
+                        } catch (InterruptedException e) {
+                            e.printStackTrace();
+                        }
+
+                        // reset pending text pointer
+                        if (currentChar != '\n') {
+                            int k = pendingText.lastIndexOf(' ');
+                            if (k == pendingText.length() - 1) {
+                                // NOTE: Special case where the pending text has a space
+                                // right where the length of the text pushes it into the next line
+                                String temp = pendingText.substring(0, k);
+                                k = temp.lastIndexOf(' ');
+                                if (k > 0) {
+                                    pendingText = temp.substring(k);
+                                    //Gdx.app.log(TAG, "0.5. set pending text = " + pendingText);
+                                }
+                                addSpaceToPendingText = true;
+                            }
+                            else if (k >= 0) {
+                                pendingText = pendingText.substring(k);
+                                //Gdx.app.log(TAG, "1. set pending text = " + pendingText);
+                            }
+                        }
+                        else if (currentChar == '\n' && numLines > 2) {
+                            // this is the end of what we need to display for this box
+                            // so set current text minus the EOL character
+                            if (numLines > 3) {
+                                // special case. set EOL to true so next iteration this gets flagged correctly
+                                int k = pendingText.lastIndexOf(' ');
+                                if (k >= 0) {
+                                    pendingText = pendingText.substring(k);
+                                    isEndOfLine = true;
+                                    numLines = 3;
+                                    //Gdx.app.log(TAG, "2. set pending text = " + pendingText);
+                                    continue;
+                                }
+                            }
+                            else
+                            {
+                                int k = pendingText.lastIndexOf('\n');
+                                if (k >= 0) {
+                                    currentVisibleText = pendingText.substring(0, pendingText.lastIndexOf('\n'));
+                                    textArea.setText(currentVisibleText);
+                                    //Gdx.app.log(TAG, "3. set currentVisibleText = " + currentVisibleText);
+                                }
+                                pendingText = "";
+                                //Gdx.app.log(TAG, "3. set pending text = " + pendingText);
+                                continue;
+                            }
+                        }
+                        else {
+                            pendingText = "";
+                            //Gdx.app.log(TAG, "4. set pending text = " + pendingText);
+                        }
+
+                        currentVisibleText = "";
+                        textArea.setText(currentVisibleText);
+                    }
+                    else {
+                        currentVisibleText = pendingText;
+                    }
+
+                    try {
+                        Thread.sleep(50);
+                    } catch (InterruptedException e) {
+                        e.printStackTrace();
+                    }
+                }
+            }
+        };
+
+        new Thread(r).start();
     }
 }
